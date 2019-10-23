5be8288c6e9fbc891f3de330088cdf69ae9f39f3
d3m1rt45 <d3m1rt45@outlook.com>
2019-10-23 15:51:24 +0100

    Initial commit

---
+++ KonneyTM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KonneyTM.Startup))]
+namespace KonneyTM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
