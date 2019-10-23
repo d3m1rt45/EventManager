489fc26d1ef5f4255808efd8014dbabee3d0c3c5
d3m1rt45 <d3m1rt45@outlook.com>
2019-10-23 18:49:18 +0100

    Welcome Pane
    Welcome Pane has been set up according to the initial design.
    
    Text alignment is yet to be corrected.

--- KonneyTM/App_Start/BundleConfig.cs
+++ KonneyTM/App_Start/BundleConfig.cs
@@ -24,7 +24,10 @@ namespace KonneyTM
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/SharedStyles.css"));
+
+            bundles.Add(new StyleBundle("~/HomeStyles").Include(
+                      "~/Content/HomeStyles.css"));
         }
     }
 }
