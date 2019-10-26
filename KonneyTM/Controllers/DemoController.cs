037aca65ae83ac7ffadb22d63bb7d34601a1e8f2
d3m1rt45 <d3m1rt45@outlook.com>
2019-10-26 18:48:21 +0100

    Database Context Targeted
    I have create a new controller named Demo Controller that will deal with the demo run's functionality. I AM NOT SURE that this is the proper way of dealing with what's at hand but will look for problems I may have with this to decide whether to take it another direction.
    
    I will work on Events, People and Venues pages in their own branches which are yet to be created.

---
+++ KonneyTM/Controllers/DemoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KonneyTM.Controllers
+{
+    public class DemoController : Controller
+    {
+        public ActionResult Events()
+        {
+            return View();
+        }
+
+        public ActionResult People()
+        {
+            return View();
+        }
+
+        public ActionResult Venues()
+        {
+            return View();
+        }
+    }
+}
