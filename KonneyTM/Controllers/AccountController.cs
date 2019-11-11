f7f25a6eadfe77cc5bfcaa0f952d1d252593aca8
d3m1rt45 <d3m1rt45@outlook.com>
2019-11-11 19:16:19 +0000

    Log In / Log Off
    I've decided to implement user functionalities to the application and has taken the first step of enabling the links back again. Still a lot of adjustments to do and I will have to figure out how to assign events venues and people to certain users.

--- KonneyTM/Controllers/AccountController.cs
+++ KonneyTM/Controllers/AccountController.cs
@@ -79,7 +79,7 @@ namespace KonneyTM.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    return RedirectToAction("Index", "Events");
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
@@ -163,7 +163,7 @@ namespace KonneyTM.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Events");
                 }
                 AddErrors(result);
             }
